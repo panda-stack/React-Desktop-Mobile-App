a789a43dfb1b9697c3dfa413330c586de94a4b3e
panda <kongfupanda2050@gmail.com>
2020-05-04 00:46:45 +0800

    React Responsive Desktop/Mobile App

---
+++ src/layouts/left.js
@@ -0,0 +1,54 @@
+import React, {Component} from 'react';
+import '../assests/css/Left.css';
+import '../assests/css/Canvas.css';
+import Header from './header';
+import Footer from './footer';
+import { init } from '../script/script';
+import { AlarmIcon, CircleIcon, MenuIcon, PatternIcon } from '../components/svglogo';
+
+class Left extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+          width: 50,
+        };
+        this.openGoogle = this.openGoogle.bind(this);
+    }
+    
+    componentDidMount() {
+        init();
+    }
+
+    openGoogle() {
+      window.open(
+        'http://google.com',
+        '_blank'
+      );
+    }
+
+    render() {
+        return (
+          <div className="Left" id='leftwin'>
+            <Header />
+            <h3 className="lefttitle">
+                Welcome! Thanks for joining us.
+            </h3>
+            <div className="circlelogo">
+                {/* <img src={require('../assests/svg/circle.svg')} /> */}
+                <CircleIcon />
+            </div>
+            <div className="Pattern">
+                {/* <img src={require('../assests/svg/pattern.svg')} /> */}
+                <PatternIcon />
+            </div>
+            <div className="leftbtn">
+                <button onClick={this.openGoogle}>Set up your account</button>
+            </div>
+            <Footer />
+          </div>
+        );
+    }
+}
+
+export default Left;
